2dd006c587daf02aa68fcf42d735d3a13871c74b
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-05-15 09:34:29 +0700

    chore: change header ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,69 +1,101 @@
-import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState, useEffect } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Stack,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import menuConfigs from "../config/menu.configs";
-import Logo from "./Logo";
+	AppBar,
+	Box,
+	Button,
+	IconButton,
+	Stack,
+	Toolbar,
+	Typography,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+import menuConfigs from '../config/menu.configs';
+import Logo from './Logo';
 
 const Header = () => {
-  return (
-    <>
-      <AppBar elevation={0} sx={{ zIndex: 9999 }}>
-        <Toolbar className="items-center justify-between bg-bgColorBlock">
-          <Stack direction="row" spacing={1} alignItems="center">
-            <IconButton color="inherit" sx={{ mr: 2, display: { md: "none" } }}>
-              <MenuIcon />
-            </IconButton>
+	const [selectedIndex, setSelectedIndex] = useState(0);
+	const [scrolled, setScrolled] = useState(false);
 
-            <Box sx={{ display: { xs: "inline-block", md: "none" } }}>
-              <Logo />
-            </Box>
-          </Stack>
+	const handleButtonClick = (index) => {
+		setSelectedIndex(index);
+	};
 
-          {/* main menu */}
-          <Box
-            flexGrow={1}
-            alignItems="center"
-            display={{ xs: "none", md: "flex" }}
-          >
-            <Box sx={{ marginRight: "30px" }}>
-              <Logo />
-            </Box>
-            {menuConfigs.main.map((item, index) => (
-              <Button
-                key={index}
-                component={Link}
-                to={item.path}
-                variant="text"
-              >
-                <Typography className="text-white" fontWeight={600}>
-                  {item.display}
-                </Typography>
-              </Button>
-            ))}
-          </Box>
-          {/* main menu */}
+	const handleScroll = () => {
+		if (window.scrollY > 50) {
+			setScrolled(true);
+		} else {
+			setScrolled(false);
+		}
+	};
 
-          {/* user menu */}
-          <Stack spacing={3} direction="row" alignItems="center">
-            {
-              <Button className="bg-buttonColor" variant="contained">
-                Đăng nhập
-              </Button>
-            }
-          </Stack>
-          {/* user menu */}
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	return (
+		<>
+			<AppBar elevation={0} sx={{ zIndex: 9999, backgroundColor: scrolled ? '#131313' : 'transparent', transition: 'background-color 0.3s' }}>
+				<Toolbar className="items-center justify-between">
+					<Stack direction="row" spacing={1} alignItems="center">
+						<IconButton color="inherit" sx={{ mr: 2, display: { md: 'none' } }}>
+							<MenuIcon />
+						</IconButton>
+
+						<Box sx={{ display: { xs: 'inline-block', md: 'none' } }}>
+							<Logo />
+						</Box>
+					</Stack>
+
+					{/* main menu */}
+					<Box
+						flexGrow={1}
+						alignItems="center"
+						display={{ xs: 'none', md: 'flex' }}>
+						<Box sx={{ marginRight: '30px' }}>
+							<Logo />
+						</Box>
+						{menuConfigs.main.map((item, index) => (
+							<Button
+								key={index}
+								component={Link}
+								to={item.path}
+								variant="text"
+								onClick={() => handleButtonClick(index)}
+								style={{
+									backgroundColor:
+										selectedIndex === index ? 'red' : 'transparent',
+								}}>
+								<Typography
+									className="text-white"
+									fontWeight={600}
+									sx={{ fontSize: '13px' }}>
+									{item.display}
+								</Typography>
+							</Button>
+						))}
+					</Box>
+					{/* main menu */}
+
+					{/* user menu */}
+					<Stack spacing={3} direction="row" alignItems="center">
+						{
+							<Button
+								className=""
+								variant="contained"
+								sx={{ fontSize: '13px', backgroundColor: '#ff0000' }}>
+								Đăng nhập
+							</Button>
+						}
+					</Stack>
+					{/* user menu */}
+				</Toolbar>
+			</AppBar>
+		</>
+	);
 };
 
 export default Header;
