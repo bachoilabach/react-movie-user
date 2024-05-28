0be8e9423792377edf4d8ae24874dd81e56d8504
Nguyễn Việt Trường <112754228+Imtruongz@users.noreply.github.com>
2024-05-28 22:26:11 +0700

    new route for list movie

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
 						<Route path="/movie/:id" element={<MovieDetail />} />
 						<Route path="/actor/:id" element={<ActorDetail />} />
 						<Route path="/search" element={<SearchPage />} />
+						<Route path="/tv" element={<ListMovie />} />
 					</Route>
 					<Route path="*" element={<NotFound />} />
 				</Routes>
