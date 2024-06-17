e7d3cbcbbf459f74117857a838f8f74c6c3bacc6
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-06-17 16:31:46 +0700

    add: Add the feature of adding your favorite movies

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,7 +6,7 @@ const handleLoginApi = (userEmail, userPassword) => {
 	});
 };
 
-const handleSignUpApi = (userEmail, userPassword,userFullName, roleID) => {
+const handleSignUpApi = (userEmail, userPassword, userFullName, roleID) => {
 	return axios.post('/api/user/create-new-user', {
 		email: userEmail,
 		password: userPassword,
@@ -16,9 +16,31 @@ const handleSignUpApi = (userEmail, userPassword,userFullName, roleID) => {
 };
 
 const handleUpdateUserDataApi = (userData) => {
-	console.log(userData)
+	console.log(userData);
 	return axios.put('/api/user/edit-user', {
 		user: userData,
+	});
+};
+
+const handleGetFavouriteMovie = (email) => {
+	return axios.get(`/api/favourite/favourite-movies?email=${email}`, {
+		email: email,
+	});
+};
+
+const handleAddFavouriteMovie = (email, movieID) => {
+	return axios.post('/api/favourite/add-favourite-movie', {
+		email: email,
+		movieID: movieID,
+	});
+};
+
+const handleDeleteFavouriteMovie = (email, movieID) => {
+	return axios.delete('/api/favourite/delete-favourite-movie', {
+		data: {
+			email: email,
+			movieID: movieID,
+		},
 	});
 };
 
@@ -26,4 +48,7 @@ export {
 	handleLoginApi,
 	handleSignUpApi,
 	handleUpdateUserDataApi,
+	handleGetFavouriteMovie,
+	handleAddFavouriteMovie,
+	handleDeleteFavouriteMovie,
 };
